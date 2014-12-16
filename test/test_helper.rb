9a2cfd0f2c52286f8da5ff5add5bac2b68fff4a9
Jikku Jose <jikkujose@gmail.com>
2014-12-16 08:43:22 +0530

    Add minitest

---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require_relative './xp_test.rb'
+
+reporter_options = { color: true, slow_count: 5 }
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
