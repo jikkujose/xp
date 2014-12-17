1d8c0a56fe11aa604f825c58ad1bad9effbaf1e0
Jikku Jose <jikkujose@gmail.com>
2014-12-17 12:47:35 +0530

    Add user agents in separate file

---
+++ lib/user_agents.rb
@@ -0,0 +1,10 @@
+module XP
+  USER_AGENTS = {
+    ipad: 'Mozilla/5.0 (iPad; CPU OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B176 Safari/7534.48.3',
+    iphone: 'Mozilla/5.0 (iPhone; CPU OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11B554a Safari/9537.53',
+    mac_firefox: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14',
+    mac_safari: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14'
+  }
+
+  USER_AGENTS.default = USER_AGENTS[:mac_safari]
+end
