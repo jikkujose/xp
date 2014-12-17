50fd3dff79deab6c043af442bac04d23bc3bb6c2
Jikku Jose <jikkujose@gmail.com>
2014-12-17 12:50:57 +0530

    Update UI with the tests

--- lib/xp.rb
+++ lib/xp.rb
@@ -1,17 +1,36 @@
 require 'nokogiri'
 require 'open-uri'
+require_relative './user_agents.rb'
 
 module XP
-  def filter(selector)
-    selection_method = css?(selector) ? :css : :xpath
-    Nokogiri(self).send(selection_method, selector).to_html
+  def to_nokogiri
+    Nokogiri(self)
   end
 
-  def css?(selector)
-    selector[0] != '/'
+  def page_source(user_agent_alias: :mac_firefox, user_agent: nil)
+    user_agent ||= USER_AGENTS[user_agent_alias]
+    open(self, "User-Agent" => user_agent).read
   end
 
-  def download
-    open(self).read
+  def download(location: 'downloads', name: nil)
+    FileUtils.mkdir_p location
+    filename = (name || basename).to_s + extension
+    File.open("#{location}/#{filename}", 'wb') do |f|
+      f.write open(self).read
+    end
+  end
+
+  def basename
+    self.match(regex)[:basename]
+  end
+
+  def extension
+    self.match(regex)[:extension]
+  end
+
+  def regex
+    /.*\/(?<basename>.*)(?<extension>\.\w+)(\?.*)?/
   end
 end
+
+String.send :include, XP
--- test/xp_test.rb
+++ test/xp_test.rb
@@ -1,10 +1,17 @@
 require './lib/xp'
 
+HTML = File.open('./test/test_content.html').read
 XPATH_QUERY = '//title'
 XPATH_RESULT = "<title>How You Know</title>\n"
 
 CSS_QUERY = 'title'
 CSS_RESULT = "<title>How You Know</title>\n"
+
+SIMPLE_PAGE_URL = "http://w3m.sourceforge.net/"
+TEST_URL = 'http://qucentis.com/unavailable_404_link'
+TEST_FILE_URL = 'http://abc.com/file/hello.mov?key=123&id=569'
+TEST_FILE_BASENAME = 'hello'
+TEST_FILE_EXTENSION = '.mov'
 
 class TestXP < Minitest::Test
   String.send(:include, XP)
@@ -13,11 +20,28 @@ class TestXP < Minitest::Test
     @html = File.open('./test/test_content.html').read
   end
 
-  def test_filter_with_css_selector
-    assert_equal CSS_RESULT, @html.filter(CSS_QUERY)
+  def test_all_methods_introduced
+    %w|to_nokogiri page_source download|.each do |method|
+      assert_respond_to "http://google.com", method
+    end
   end
 
-  def test_filter_with_xpath_selector
-    assert_equal XPATH_RESULT, @html.filter(XPATH_QUERY)
+  def test_to_nokogiri
+    assert_instance_of Nokogiri::HTML::Document, HTML.to_nokogiri
+  end
+
+  def test_conversion_to_nodeset
+    assert_equal HTML.to_nokogiri.css(CSS_QUERY).to_html, CSS_RESULT
+    assert_equal HTML.to_nokogiri.css(XPATH_QUERY).to_html, XPATH_RESULT
+    assert_equal HTML.to_nokogiri.css(XPATH_QUERY).xpath("//title/text()").to_html, "How You Know"
+  end
+
+  def test_page_source
+    assert_equal SIMPLE_PAGE_URL.page_source.to_nokogiri.xpath("//address/a/text()").to_html, "aito@fw.ipsj.or.jp"
+  end
+
+  def test_basename
+    assert_equal TEST_FILE_URL.basename, TEST_FILE_BASENAME
+    assert_equal TEST_FILE_URL.extension, TEST_FILE_EXTENSION
   end
 end
